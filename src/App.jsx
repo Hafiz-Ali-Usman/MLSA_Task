5812045aabe5fa034022d161e41962e81787235a
Ali Usman <122893647+Hafiz-Ali-Usman@users.noreply.github.com>
2024-05-27 17:30:25 +0500

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Hafiz Ali Usman",
+  title: "Machine Learning Engineer",
+  email: "HafizAli.Usman@studentambassadors.com",
+  gitHub: "https://github.com/Hafiz-Ali-Usman",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/ali-usman-5b287a230/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
